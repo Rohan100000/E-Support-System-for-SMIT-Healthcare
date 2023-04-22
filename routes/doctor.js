2a21d19161375bb518d4c6a3e5bd58ba36b61fab
uwu_rimjhim <rimjhimshrivastava19@gmail.com>
2023-04-22 15:43:11 +0530

    v0.12.7: create prescription controller added

--- routes/doctor.js
+++ routes/doctor.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const doctorController = require('../controllers/doctor_controller');
 
-router.get('/profile',passport.checkAuthentication,doctorController.profile);
-router.get('/profile_patient/:id',passport.checkAuthentication,doctorController.profile_patient);
-
+router.get('/profile', passport.checkAuthentication, doctorController.profile);
+router.get('/profile_patient/:id', passport.checkAuthentication, doctorController.profile_patient);
+router.post('/create-prescription/:id', passport.checkAuthentication, doctorController.createPrescription)
 module.exports = router;
